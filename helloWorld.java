9608f2e02dcb871658abf0327984e6e26c8ebe05
Kushal Gopee <firewaterkushal@gmail.com>
2020-09-15 16:35:14 +0200

    new project tested

--- helloWorld.java
+++ helloWorld.java
@@ -1,10 +1,13 @@
-
+import java.util.Scanner;
 
 public class helloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Git is awesome");
-
+		Scanner user_input = new Scanner(System.in);
+		System.out.println("Type something ");
+		String user_message = user_input.next();
+		System.out.println("You typed : " + user_message);
+		user_input.close();
 
 	}
 
